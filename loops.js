900b8ebbdefa507407a2c7da32f8596d453b93a6
nbonheur111 <56658293+nbonheur111@users.noreply.github.com>
2022-11-20 21:45:25 -0600

    first four questions

---
+++ loops.js
@@ -0,0 +1,20 @@
+//2. loop that logs 0 - 15.
+for(let i = 0; i <=15; i++){
+    console.log(i);
+}
+console.log("");
+// 3. loop that counts from 15 - 0
+for(i = 15; i >= 0; i--){
+    console.log(i);
+}
+// 4. add 1 to the laps variable. AFTER the loop, log laps.
+
+laps = 0;
+
+for(let i = 0; i < 5; i++){
+    console.log("that's another lap");
+    laps+=1;
+}
+console.log(`laps = ${laps}`);
+
+
